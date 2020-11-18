0b207047588eb5cd7954738cc5e8e31a1a771490
Arash Afshar <arash.afshar.edu@gmail.com>
2020-11-17 20:31:46 -0700

    added data processing and simple unit test

--- backend/src/dataset/app.py
+++ backend/src/dataset/app.py
@@ -1,5 +1,55 @@
+import json
+
 class Main:
 
     @staticmethod
     def run():
         print("Hello World...")
+
+    def _get_keywords(self, body):
+        prefix = "Category / Keywords: "
+        for item in body:
+            if item["body"].startswith(prefix):
+                return item["body"][len(prefix):].strip()
+        return "Not found"
+    
+    def _get_extension(self, body):
+        prefix = "Available format(s):"
+        for item in body:
+            if item["body"].startswith(prefix):
+                content = item["body"][len(prefix):].lower()
+                if "pdf" in content:
+                    return "pdf"
+                if "ps" in content:
+                    return "ps"
+        return "invalid"
+    
+    def _get_abstract(self, body):
+        prefix = "Abstract: "
+        abstract = ""
+        for item in body:
+            if item["body"].startswith("Category / Keywords: "):
+                break
+            abstract += item["body"]
+        return abstract[len(prefix):].strip()
+
+    def _cleanup_title(self, title):
+        try:
+            title = str(title)
+            title = title.strip()
+            title = ' '.join(title.split())
+            return title
+        except Exception as e:
+            print(e)
+
+    def process(self, articles):
+        articles['body'] = articles['body'].apply(json.loads)
+        articles['extension'] = articles['body'].apply(self._get_extension)
+        articles['abstract'] = articles['body'].apply(self._get_abstract)
+        articles['keywords'] = articles['body'].apply(self._get_keywords)
+        articles['url'] = articles['article-href'] + "." + articles['extension']
+        articles['title'] = articles['title'].apply(self._cleanup_title)
+        articles['authors'] = articles['authors'].apply(str)
+        articles = articles.drop(['article-href', 'body', 'extension', 'web-scraper-order', 'web-scraper-start-url', 'article'], axis=1)
+        return articles
+
--- backend/tests/dataset/test_app.py
+++ backend/tests/dataset/test_app.py
@@ -1,9 +1,30 @@
 from tests.context import dataset
+import pandas as pd
 
 
 def test_app(capsys, example_fixture):
     # pylint: disable=W0612,W0613
-    dataset.Main.run()
-    captured = capsys.readouterr()
 
-    assert "Hello World..." in captured.out
+    df = pd.DataFrame({
+        "web-scraper-order": ["aaa"],
+        "web-scraper-start-url": ["bbb"],
+        "article": ["ccc"],
+        "article-href": ["https://example.com/file.name"],
+        "title": ["      Title    123     . $$$    "],
+        "authors": ["Person A and Person B"],
+        "body": ['[{"body":"Abstract: first paragraph"},{"body": "Second paragraph"},' \
+                '{"body":"Category / Keywords: keyword1 / keyword2, keyword3"},' \
+                '{"body":"aaa"},{"body":"bbb"},{"body":"Available format(s): PDF"},{"body":""}]'],
+        })
+
+    
+
+    main = dataset.Main()
+    processed = main.process(df)
+
+    assert len(processed.columns) == 5
+    assert processed['title'][0] == 'Title 123 . $$$'
+    assert processed['authors'][0] == 'Person A and Person B'
+    assert processed['abstract'][0] == 'first paragraphSecond paragraph'
+    assert processed['keywords'][0] == 'keyword1 / keyword2, keyword3'
+    assert processed['url'][0] == 'https://example.com/file.name.pdf'
